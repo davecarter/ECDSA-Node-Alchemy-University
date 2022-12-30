a4f077c5625a899befa50cefc7fd765f855dd76e
David Garcia <dave74@gmail.com>
2022-12-31 00:50:47 +0100

    generate public private key pair

--- server/index.js
+++ server/index.js
@@ -1,44 +1,54 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const port = 3042;
+const express = require("express")
+const secp = require("ethereum-cryptography/secp256k1")
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils")
 
-app.use(cors());
-app.use(express.json());
+const { getAddress } = require("./utils/generator")
+const app = express()
+const cors = require("cors")
+const port = 3042
+
+const privateKey = secp.utils.randomPrivateKey()
+const publicKey = secp.getPublicKey(privateKey)
+
+app.use(cors())
+app.use(express.json())
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
+  "3e93820f8edefc6c9961e848d9fd87dc1490190e": 100, // 460f0612dcafdcf0914fd61b7bb895a7dd5d59d664121716935174636a6bd9e5
+  "33d63c84bf4b5a96d69e11312eeb2e8e65317a46": 50, // 89958560357428a3bd70ccce5b22a45f3116415373d01f3b3f9b00bd4b521311
   "0x3": 75,
-};
+}
+
+console.log({ privateKey: toHex(privateKey) })
+console.log({ publicKey: toHex(getAddress(publicKey)) })
 
 app.get("/balance/:address", (req, res) => {
-  const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
-});
+  const { address } = req.params
+  const balance = balances[address] || 0
+  res.send({ balance })
+})
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount } = req.body
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  setInitialBalance(sender)
+  setInitialBalance(recipient)
 
   if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+    res.status(400).send({ message: "Not enough funds!" })
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    balances[sender] -= amount
+    balances[recipient] += amount
+    res.send({ balance: balances[sender] })
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+  console.log(`Listening on port ${port}!`)
+})
 
 function setInitialBalance(address) {
   if (!balances[address]) {
-    balances[address] = 0;
+    balances[address] = 0
   }
 }
